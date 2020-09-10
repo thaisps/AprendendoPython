22476be018f3b21de22d48475453baeadcd49782
thaisps <71051859+thaisps@users.noreply.github.com>
2020-09-10 15:46:09 -0300

    Add files via upload

---
+++ Ex_07.py
@@ -0,0 +1,12 @@
+
+# Exercício 07
+
+# Faça um programa que leia 5 números e informe o maior número.
+
+print("Olá! Você irá inserir um total de 5 números!")
+n1 = float(input("Insira um número."))
+n2 = float(input("Insira mais um número."))
+n3 = float(input("Insira mais um número."))
+n4 = float(input("Insira mais um número."))
+n5 = float(input("Insira mais um número."))
+print("O número "+ str(max(n1,n2,n3,n4,n5)) + " é o maior entre o 5 números inseridos!")
